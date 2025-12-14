a98ac94555cd3d287d7cc8ac48955da533b6e708
skerish <skerish@naver.com>
2025-12-14 23:50:50 +0900

    Tailwind CDN 제거 및 PostCSS 기반 빌드 구성 추가

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './index.html',
+    './index.tsx',
+    './App.tsx',
+    './components/**/*.{ts,tsx}',
+    './utils/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
